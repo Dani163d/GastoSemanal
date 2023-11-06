6126a72b86f631694af74cf195d404df91321738
daniela <danielasilvacastillo163@gmail.com>
2023-11-06 15:06:00 -0400

    se crearon las instancias

--- js/app.js
+++ js/app.js
@@ -12,8 +12,21 @@ function eventListeners() {
 
 
 // Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI {
+    
+}
 
+// instanciar
+const UI = new UI();
+let presupuesto;
 
 
 
@@ -22,12 +35,17 @@ function eventListeners() {
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿cual es tu presupuesto?');
 
-    console.log(Number(presupuestoUsuario));
+    // console.log(Number(presupuestoUsuario));
 
 
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload()
     }
+
+
+        // presupuesto valido
+        presupuesto = new Presupuesto(presupuestoUsuario);
+        console.log(presupuesto);
 }
 
 
